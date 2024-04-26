c5a989b370d9807d3d75fe1cc9ea640ae3eeb993
blankuet <blancafeijoocruz@gmail.com>
2024-04-26 19:37:17 +0200

    class Game created, start method done + music button in progress

--- js/game.js
+++ js/game.js
@@ -1,1 +1,12 @@
-//Create class GAME with methods
+class Game {
+    constructor() {
+        this.startScreen = document.querySelector('#game-intro');
+        this.gameScreen = document.querySelector('#game-screen');
+        this.endScreen = document.querySelector('#game-end');
+    }
+
+    start() {
+        this.startScreen.style.display = 'none';
+        this.gameScreen.style.display = 'block';
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-// Create functions
+window.onload = function (){
+    const startButton = document.getElementById("start-button");
+    const musicButton = document.getElementById("music-button");
+
+    let game;
+
+    startButton.addEventListener("click", function() {
+        startGame();
+    });
+
+    function startGame(){
+        game = new Game();
+        game.start();
+    }
+
+    musicButton.addEventListener("click", function() {
+        musicButton.textContent = "Music off 🔇";
+        //PENDING HOW TO CHANGE IT BACK TO 
+    });
+}
